48b777e4c6b7e3c900656b93b01c3739557a169c
otsomyk <oksana.tsomyk@ukr.net>
2015-10-26 12:49:31 +0200

    Update .gitignore

---
+++ GreetingService2.java
@@ -0,0 +1,13 @@
+//package day4;
+
+public class GreetingService2 {
+
+	public static void main(String[] args) {
+	if(args.length > 0){
+		System.out.println("Hello " + args[0]);
+	} else {
+	System.out.println("Add the word, please. Example: Hello world!");
+	}
+}
+	
+}
